f055e289afa5e50344648596c865321d725d50f0
Joseph Fischetti <fischettijw@gmail.com>
2021-08-05 12:26:33 -0400

    update

---
+++ python_starting_point.py
@@ -0,0 +1,27 @@
+# region Links
+
+# https://www.youtube.com/watch?v=g_wlZ9IhbTs
+
+# endregion Links
+
+# region Imports
+
+import os
+os.system('cls')
+# endregion Imports
+
+# region functions
+
+
+# endregion Functions
+
+# region main function
+
+def main():
+    print("Hello, world! from 'function main()'\n")
+
+# endregion main function
+
+
+if __name__ == '__main__':
+    main()
